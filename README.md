dd9fd0b337d8d82e39e0e5546c32f0e1848fa317
meron <malemu17@yahoo.com>
2019-11-05 16:57:58 -0800

    appaccessedViagithub

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# lab-11
-OAuth
+# LAB - 
+
+## OAuth
+
+### Author: Meron
+
+### Links and Resources
+* [submission PR](http://xyz.com)
+* [travis](http://xyz.com)
+
+#### Documentation
+* 
+
+### Setup
+#### `.env` requirements
+* `PORT` - 3000
+* `MONGODB_URI` - mongodb+srv://meron123:meron123@cfcluster-kexaa.mongodb.net/app?retryWrites=true&w=majority
+
+#### Running the app
+* `npm start` or `nodemon index.js`
+
+  
+#### Tests
+* How do you run tests?
+* What assertions were made?
+* What assertions need to be / should be made?
+
+#### UML
+Link to an image of the UML for your application and response to events
+
+
+
---
+++ src/routes/oauth/github.js
@@ -0,0 +1,38 @@
+'use strict';
+
+const express = require('express');
+
+const githubMW = require('../../middleware/oauth/github-mw.js');
+const router = express.Router();
+
+router.get('/github', (req, res, next) => {
+  //console.log('in the /github route');
+  let githubOAuthUrl = process.env.GITHUB_AUTH_SERVICE;
+
+  githubOAuthUrl += '?';
+  let options = {
+    client_id:process.env.GITHUB_CLIENT_ID,
+    redirect_uri:process.env.HOME_URL + '/github-oauth',
+    login:'admin',
+    scope: 'user',
+    state: 'random',
+    allow_signup: true
+  }
+  Object.keys(options).forEach((key, indx) => {
+   githubOAuthUrl += key + '=' + encodeURIComponent(options[key]);
+   githubOAuthUrl += '&';
+   
+   //console.log(githubOAuthUrl)
+   
+   res.status(200).json({url:githubOAuthUrl})
+  });
+});
+// query urls:
+// https://github.com/login/oauth/authorize?client_id=d79b4faed9667d24e532&redirect_uri=localhost:1234/path
+
+ 
+
+
+router.get('/github-oauth', async (req, res, next) => {});
+
+module.exports = router;
